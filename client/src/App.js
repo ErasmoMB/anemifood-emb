15a61a8456e208f5c63bf0466dbff16730418cbc
emb <montufarbe@gmail.com>
2023-12-09 00:53:38 -0500

    revisar urls

--- client/src/App.js
+++ client/src/App.js
@@ -10,30 +10,32 @@ import Cancel from "./pages/Cancel";
 import CartProvider from "./Cart";
 
 // Accede a la variable de entorno REACT_APP_BACKEND_URL
-const backendURL = process.env.REACT_APP_BACKEND_URL || "http://localhost:4000";
-const frontendURL =
-  process.env.REACT_APP_FRONTEND_URL || "http://localhost:3000";
+/* const backendURL = process.env.BACKEND || "http://localhost:4000";
+const frontendURL = process.env.FRONTEND || "http://localhost:3000";
 const successURL = `${frontendURL}/success`;
 const cancelURL = `${frontendURL}/cancel`;
-
+ */
 function App() {
   return (
     <div>
       <CartProvider>
         {/* Pasa la variable de entorno a Navbar */}
-        <Navbar backendURL={backendURL} />
+        <Navbar /* backendURL={backendURL} */ />
         <div className="container">
           <BrowserRouter>
             <Routes>
               {/* Pasa la variable de entorno a Store */}
-              <Route path="/" element={<Store backendURL={backendURL} />} />
+              <Route
+                path="/"
+                element={<Store /* backendURL={backendURL}  */ />}
+              />
               <Route
                 path="/success"
-                element={<Success successURL={successURL} />}
+                element={<Success /* successURL={successURL} */ />}
               />
               <Route
                 path="/cancel"
-                element={<Cancel cancelURL={cancelURL} />}
+                element={<Cancel /* cancelURL={cancelURL} */ />}
               />
             </Routes>
           </BrowserRouter>
