12ba560f5cc597f3ad616d9594178f17ce778d4b
emb <montufarbe@gmail.com>
2023-12-09 00:20:59 -0500

    correcion de url

--- client/src/pages/Success.js
+++ client/src/pages/Success.js
@@ -1,12 +1,10 @@
-import { Link } from "react-router-dom";
-
 function Success() {
   return (
     <div>
       <h1 className="mt-3">Thanks for your purchase!</h1>
-      <Link to="/" className="btn btn-success mt-2">
+      <a href="/" className="btn btn-success mt-2">
         Back to home
-      </Link>
+      </a>
     </div>
   );
 }
